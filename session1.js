a3b6a4defeada4fbe0cbd74113751143d823262f
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-09 16:34:33 -0500

    Solution examples for "reverse an integer" challenge.

--- session1.js
+++ session1.js
@@ -1,27 +1,18 @@
-// CHALLENGE 2: VALIDATE A PALINDROME
-// Return TRUE if palindrome, FALSE if not
-// ex. isPalindrome('racecar') === true, isPalindrome('hello') == false
+// Challenge 3: Reverse an Integer
+// Return an integer in reverse - reverseInt(521) === 125
 
-// initial solution
-// function isPalindrome(str) {
-//   // lower case to equalize the input/output
-//   // split to get an array
-//   // reduce to swap/reverse characters
-//   return (
-//     str
-//       .toLowerCase()
-//       .split("")
-//       .reduce((char, nextChar) => nextChar + char) === str
-//   );
-// }
-
-// more verbose solution using reverse and join
-// function isPalindrome(str) {
-//   return (
-//     str
-//       .toLowerCase()
-//       .split("")
-//       .reverse()
-//       .join("") === str
-//   );
-// }
+// simple solution
+function reverseInt(int) {
+  //convert, split, reverse, join
+  //wrap entire chain in parseInt since we need to RETURN AN INT
+  // math.sign allows us to keep the number positive or negative = returns -1 if negative, so return * -1.
+  return (
+    parseInt(
+      int
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ) * Math.sign(int)
+  );
+}
