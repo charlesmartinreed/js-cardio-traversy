f96a01a99f13411643973fba10643faef8373ed1
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-09 17:39:16 -0500

    Solution examples for "capitalize first letter" challenge.

--- session1.js
+++ session1.js
@@ -1,18 +1,53 @@
-// Challenge 3: Reverse an Integer
-// Return an integer in reverse - reverseInt(521) === 125
+// CHALLENGE 4: CAPITALIZE LETTERS
+// Return a string with the first letter of every word capitalized
 
-// simple solution
-function reverseInt(int) {
-  //convert, split, reverse, join
-  //wrap entire chain in parseInt since we need to RETURN AN INT
-  // math.sign allows us to keep the number positive or negative = returns -1 if negative, so return * -1.
-  return (
-    parseInt(
-      int
-        .toString()
-        .split("")
-        .reverse()
-        .join("")
-    ) * Math.sign(int)
-  );
+// simple for-loop
+// function capitalizeLetters(str) {
+//   const strArr = str.toLowerCase().split(" ");
+//
+//   for (let i = 0; i < strArr.length; i++) {
+//     // take the first letter, uppercase it, add the rest of the substring, from index 1 forward
+//     strArr[i] =
+//       strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+//   }
+//
+//   return strArr.join(" ");
+// }
+
+// using map
+// function capitalizeLetters(str) {
+//   return str
+//     .toLowerCase()
+//     .split(" ")
+//     .map(word => word[0].toUpperCase() + word.substr(1))
+//     .join(" ");
+// }
+
+// using regEx
+function capitalizeLetters(str) {
+  // regEx need to be within / /
+  // /b is word boundary, [a-z] is alpha from a-z, g for global, i for case insensitive
+  // second param is a function
+  return str.replace(/\b[a-z]/gi, char => char.toUpperCase());
 }
+
+// // CHALLENGE 5: MAX CHARACTER
+// // Return the character that is most common in a string
+// // ex: maxCharacter('javascript') == 'a'
+//
+// function maxCharacter(str) {
+//   let characters = {};
+//
+//   for (let char in str) {
+//     if (!characters[str[char]]) {
+//       characters[str[char]] = 1;
+//     } else {
+//       characters[str[char]] += 1;
+//     }
+//   }
+//
+//   return Object.keys(characters).reduce((acc, next) => {
+//     let val = characters[acc];
+//     return val;
+//   });
+// }
